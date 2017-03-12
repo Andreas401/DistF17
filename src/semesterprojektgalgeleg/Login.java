c84a5fab637e23f6ddd959fa23a50b7c100864a1
RyuuPham <RyuuPham@192.168.0.178>
2017-03-12 02:44:59 +0100

    stanard player with improved OI

--- src/semesterprojektgalgeleg/Login.java
+++ src/semesterprojektgalgeleg/Login.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package semesterprojektgalgeleg;
 
 import brugerautorisation.transport.soap.Brugeradmin;
@@ -18,29 +18,39 @@ import javax.xml.ws.Service;
  */
 public class Login {
     
+    String fornavn = "";
+    String efternavn = "";
     
     public void logintjek() throws MalformedURLException {
         URL url = new URL("http://javabog.dk:9901/brugeradmin?wsdl");
-	QName qname = new QName("http://soap.transport.brugerautorisation/", "BrugeradminImplService");
-	Service service = Service.create(url, qname);
-        Brugeradmin brugertjek = service.getPort(Brugeradmin.class);
+        QName qname = new QName("http://soap.transport.brugerautorisation/", "BrugeradminImplService");
+        Service service = Service.create(url, qname);
+        Brugeradmin ba = service.getPort(Brugeradmin.class);
         boolean tjek = false;
         while(tjek == false){
-        System.out.println("Enter username");
-         Scanner scan = new Scanner(System.in);
-         String username = scan.nextLine();
-         
-         System.out.println("Enter password");
-         Scanner scan2 = new Scanner(System.in);
-         String password = scan.nextLine();
-         
-        if(brugertjek.hentBruger(username, password)!=null){
-            System.out.println("Du er logget ind!");
-            tjek = true;
-        }else{
-            System.out.println("prøv igen!");
-        }}
+            System.out.println("Enter username");
+            Scanner scan = new Scanner(System.in);
+            String username = scan.nextLine();
+            
+            System.out.println("Enter password");
+            Scanner scan2 = new Scanner(System.in);
+            String password = scan.nextLine();
+            
+            if(ba.hentBruger(username, password)!=null){
+                fornavn = ba.hentBruger(username, password).fornavn;
+                efternavn = ba.hentBruger(username, password).efternavn;
+                
+                System.out.println("Du er logget ind som " + hentNavn());
+                tjek = true;
+            }else{
+                System.out.println("prøv igen!");
+            }}
     }
-         
+    
+    
+    public String hentNavn(){ 
+        return fornavn + " " + efternavn;
+    }
+    
     
 }
--- src/semesterprojektgalgeleg/StandardPlayer.java
+++ src/semesterprojektgalgeleg/StandardPlayer.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package semesterprojektgalgeleg;
 
 
@@ -21,38 +21,46 @@ public class StandardPlayer {
         
         // localhost bruges til test på egen computer og ubuntu4 bruges til når du tester
         // med anden server.
-        URL url = new URL("http://ubuntu4.javabog.dk:9978/galgeleg?wsdl");
-         // URL url = new URL("http://localhost:9978/galgeleg?wsdl");
-		QName qname = new QName("http://server/", "GameLogicService");
-                Service service = Service.create(url, qname);
-                MainInterface i = service.getPort(MainInterface.class);
-                    i.nulstil();
-                Login logintjek = new Login();
-                
-                logintjek.logintjek();
-                while(i.erSpilletSlut() == false){
-                    System.out.println("" + i.getSynligtOrd());
-                    i.logStatus();
-                System.out.println("Indtast bogstav her: ");
-                Scanner scan = new Scanner(System.in);
-                String letter = scan.nextLine();
-                
-                i.gætBogstav(letter);
-                
-              
-                if(i.erSpilletTabt() == true){
-                    System.out.println("Spillet er tabt!");
-                } else if(i.erSpilletVundet() == true){
-                    System.out.println("Spillet er vundet!");
-                } else {
-                    System.out.println("SENT");
-                }
-                
-                }    
-                
-                        
-                    
-                
-                
+        //URL url = new URL("http://ubuntu4.javabog.dk:9978/galgeleg?wsdl");
+        URL url = new URL("http://localhost:9978/galgeleg?wsdl");
+        QName qname = new QName("http://server/", "GameLogicService");
+        Service service = Service.create(url, qname);
+        MainInterface i = service.getPort(MainInterface.class);
+        i.nulstil();
+        try {
+            i.hentOrdFraDr();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        Login l = new Login();
+        
+        l.logintjek();
+        while(i.erSpilletSlut() == false){
+            System.out.println("" + i.getSynligtOrd());
+            i.logStatus();
+            System.out.println("Du har " + i.hentAntalForsoeg() + " forsøg tilbage!" );
+            System.out.println("Du har brugt disse bogstaver: " + i.getBrugteBogstaver());
+            
+            System.out.println("Indtast bogstav her: ");
+            Scanner scan = new Scanner(System.in);
+            String letter = scan.nextLine();
+            
+            i.gætBogstav(letter.toLowerCase());
+            
+            System.out.println(l.hentNavn() + " gætter på: " + letter);
+            
+            
+        }
+        if(i.erSpilletTabt() == true){
+            System.out.println("Spillet er tabt!");
+        }
+        if(i.erSpilletVundet() == true){
+            System.out.println("Spillet er vundet!");
+        }
+        System.out.println("\nOrdet er følgende: " + i.getOrdet());
+        
+        
+        
     }
 }
